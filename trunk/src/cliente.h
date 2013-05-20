d253ff49897fd71b5a918d5159d4d6fcffe90147
collinetjorge@gmail.com <collinetjorge@gmail.com@b2cfa7c8-117a-d20a-678d-662d165223aa>
2013-05-20 18:56:48 +0000

    sigo agregando funciones basicas

--- trunk/src/cliente.h
+++ trunk/src/cliente.h
@@ -8,6 +8,7 @@
 #ifndef CLIENTE_H_
 #define CLIENTE_H_
 #include <string>
+#include <vector>
 /**
  * @class Cliente cliente.h
  * @brief Clase que se encargará de las acciones generales que puede hace un cliente
@@ -16,17 +17,16 @@ class Cliente {
 
 public:
 	/**
-	 * @brief Intentará iniciar sesion con el usuario y contraseña ingresados,
-	 * internamente se comunicarà con el servidor y este le responderá si el
-	 * usuario y contraseña son correctos. Si devuelve bool es que el login
-	 * fue exitoso en caso contrario devuelve false.
+	 * @brief Intentará iniciar sesion con el usuario y contraseña ingresados
+	 * @details Se comunicarà con el servidor y este le responderá si el usuario y contraseña son correctos
+	 * @return True si el login fue exitoso, false en caso contrario
 	 */
 	bool login(std::string usuario, std::string contrasenia);
 	/** @brief POSIBLE FUNCION A IMPLEMENTAR */
 	void logout();
 	/** @brief Setea el directorio en donde se guardan los archivos del usuario */
 	void set_directorio(const char* dir);
-
+	std::vector<std::string> polling();
 };
 
 

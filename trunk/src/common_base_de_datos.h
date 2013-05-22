78b2512a01d599822bb83ed6062cb7a813edc08b
collinetjorge@gmail.com <collinetjorge@gmail.com@b2cfa7c8-117a-d20a-678d-662d165223aa>
2013-05-22 23:24:17 +0000

    hemm bueno el error seguia ahora apuesto 3 peñiques a que lo solucione!!

--- trunk/src/common_base_de_datos.h
+++ trunk/src/common_base_de_datos.h
@@ -7,16 +7,23 @@
 
 #ifndef COMMON_BASE_DE_DATOS_H_
 #define COMMON_BASE_DE_DATOS_H_
+#include <vector>
 #include "common_modificacion.h"
-using namespace std;
+
 class BaseDeDatos {
 public:
 	bool set_directorio(std::string dir);
 	bool armar_indice_local();
 	/**@brief se encarga de encontrar los archivos modificados y
 	 *  generar un vector de modificaciones*/
-	vector<Modificacion> comprobar_cambios_locales();
+	std::vector<Modificacion> comprobar_cambios_locales();
+	/**@brief agrega un archivo nuevo
+	 * @details modifica indice para que tenga al nuevo archivo , y le calcula su hash/hashes
+	 */
 	bool agregar_archivo(std::string nombre_archivo, std::string datos);
+	/**@brief elimina un archivo del directorio
+	 * @details elimina los datos del indice con de ese archivo eliminado
+	 */
 	bool eliminar_archivo(std::string nombre_archivo);
 	bool modificar_archvivo();
 };

add9b24d016910bf81e79b0ae88536815367f740
collinetjorge <collinetjorge@gmail.com>
2013-05-22 01:35:08 +0000

    tareas a realizar, y algun extra

--- src/cliente.h
+++ src/cliente.h
@@ -9,6 +9,7 @@
 #define CLIENTE_H_
 #include <string>
 #include <vector>
+#include "modificacion.h"
 /**
  * @class Cliente cliente.h
  * @brief Clase que se encargará de las acciones generales que puede hace un cliente
@@ -22,13 +23,39 @@ public:
 	 * @return True si el login fue exitoso, false en caso contrario
 	 */
 	bool login(std::string usuario, std::string contrasenia);
+
 	/** @brief POSIBLE FUNCION A IMPLEMENTAR */
 	void logout();
+
 	/** @brief Setea el directorio en donde se guardan los archivos del usuario */
 	void set_directorio(const char* dir);
-	std::vector<std::string> polling();
+
+	/**@brief Recorre los archivos en busca de modificaciones a cada modificacion
+	 * encontrada se genera una nueva instancia de la clase Modificacion y se agrega
+	 * al vector
+	 */
+	std::vector<Modificacion> polling();
+
+	/**@brief envia las modificaciones encontradas en los propios archivos*/
+	bool enviar_modificaciones(std::vector<Modificacion> modifcaciones);
+
+	/**@brief pide lista de modificaciones al servidor */
+	bool recibir_modificaciones();
+
+	/**@brief ejecuta las modificaciones que pidió anteriormente*/
+	bool ejecutar_modificaciones();
+
+	/**@brief pide el archivo cuyo nombre es ingresado al server
+	 * @details pide el archivo al servidor y lo guarda en su correspondiente lugar
+	 *  utilizando a la clase BaseDeDatos
+	 * @return devuelve true si toda la operacion fue un exito, caso contrario devuelve false */
+	bool pedir_nuevo_archivo(std::string& nombre_archivo);
+
+	/**@brief enviar el archivo cuyo nombre es ingresado
+	 * @details el archivo a enviar será abierto y a medida que se lee se envia
+	 * @return devuelve true si la operacion fue un exito, caso contrario devuelve false */
+	bool enviar_archivo(std::string& nombre_archivo);
+
 };
-
-
 
 #endif /* CLIENTE_H_ */
---
+++ src/modificacion.h
@@ -0,0 +1,31 @@
+/*
+ * modificacion.h
+ *
+ *  Created on: 20/05/2013
+ *      Author: jorge
+ */
+
+#ifndef MODIFICACION_H_
+#define MODIFICACION_H_
+#include <string>
+#include "cliente.h"
+class Cliente;
+class Modificacion {
+public:
+	/**@brief metodo que convierte a la instancia en un string para luego ser
+	 * pasada a desserializar y volver a ser una instancia pero del ordenador
+	 * en donde llegó el mensaje
+	 */
+	std::string serializar();
+	/**@brief interpreta al string recibido modifica los atributos internos de
+	 * acuerdo al string recibido
+	 */
+	void desserializar(std::string);
+
+	/**@brief despues de modificar sus atributos internos con desserializar,
+	 *  este metodo hace efectivo esa modificacion del otro lado */
+	bool efectuar_cambios(Cliente& cliente);
+};
+
+
+#endif /* MODIFICACION_H_ */

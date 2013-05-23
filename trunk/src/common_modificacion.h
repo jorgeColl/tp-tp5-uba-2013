cf74b8f0424584a2f7f1724aba81403fa7781b1c
collinetjorge@gmail.com <collinetjorge@gmail.com@b2cfa7c8-117a-d20a-678d-662d165223aa>
2013-05-23 02:08:39 +0000

    hemmmm bueno tengo otro bug pero debe ser una boludez, te mando mail para comentarte ->implementacion de gran parte del codigo de el metodo efectuar_modificaciones de la clase Modificacion ->mas agregado de cuerpo dummy en cliente_controlador para que no chille el compilador (o eso intentaba)

--- trunk/src/common_modificacion.h
+++ trunk/src/common_modificacion.h
@@ -7,11 +7,18 @@
 
 #ifndef MODIFICACION_H_
 #define MODIFICACION_H_
+
 #include <string>
 class ClienteControlador;
 
+
+enum tipo_accion{ SUBIR_NUEVO_ARCHIVO, BAJAR_NUEVO_ARCHIVO, BORRAR_ARCHIVO_LOCAL, MANDAR_A_BORRAR_ARCHIVO, SUBIR_MOD_ARCHIVO, BAJAR_MOD_ARCHIVO };
+
 class Modificacion {
+	tipo_accion accion;
+	std::string nombre_archivo;
 public:
+	Modificacion(tipo_accion accion,std::string nombre_archivo);
 	/**@brief metodo que convierte a la instancia en un string para luego ser
 	 * pasada a desserializar y volver a ser una instancia pero del ordenador
 	 * en donde llegó el mensaje

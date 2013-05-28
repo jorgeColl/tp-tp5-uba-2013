c2a98f842153677fe45ef0099e7f68dd758bba0d
cosokiter@gmail.com <cosokiter@gmail.com@b2cfa7c8-117a-d20a-678d-662d165223aa>
2013-05-28 10:03:01 +0000

    Merge a pulmon y otras yerbas

--- trunk/src/common_controlador.cpp
+++ trunk/src/common_controlador.cpp
@@ -9,53 +9,68 @@
 using namespace std;
 
 void Controlador::set_directorio(std::string dir){
-	this->dir = dir;
+        this->dir = dir;
 }
 bool Controlador::armar_indice_local() {
-	return base_de_datos.abrir(dir);
+        return base_de_datos.abrir(dir);
 }
 
 vector<Modificacion> Controlador::recibir_modificaciones() {
-	vector<Modificacion> aux;
-	return aux;
+        vector<Modificacion> aux;
+        return aux;
 }
-bool Controlador::borrar_archivo(std::string& nombre_archivo){
+
+bool Controlador::borrar_archivo(std::string& nombre_archivo)
+{
 	return base_de_datos.eliminar_archivo(nombre_archivo);
 }
-bool Controlador::mandar_a_borrar (Modificacion& mod) {
+
+bool Controlador::mandar_a_borrar (Modificacion& mod)
+{
 	/* cambio el "punto de vista" de la modificacion para que cuando el
 	server lea esto directamente borre el archivo que tiene en su directorio*/
 	mod.accion=BORRAR_ARCHIVO_LOCAL;
 
 	return sock1.enviar_modif(mod);
 }
+
+/**
+ * @brief Metodo a utilizar para pedir y recibir un archivo del servidor y agregarlo al indice
+ * @param mod Moficacion
+ * @return
+ */
 bool Controlador::pedir_nuevo_archivo(Modificacion& mod){
-	// manda mensaje a server pidiendo que transmita archivo
-	// lo guarda temporalmente en un archivo
+	// Manda mensaje a server pidiendo que transmita archivo
+	// lo guarda en un archivo temporal
 	// le dice a base de datos que guarde al nuevo archivo
 
 	// cambia el "punto de vista"
 	mod.accion =  SUBIR_NUEVO_ARCHIVO;
 	sock1.enviar_modif(mod);
 	// parte prototipo
-	ofstream* fd = base_de_datos.generar_archivo_temp(mod.nombre_archivo);
-	sock1.recibir_archivo(*fd);
-	base_de_datos.cerrar_exitosamente_archivo(mod.nombre_archivo,*fd);
+	ofstream fd;
+	base_de_datos.abrir_para_escribir_temporal(mod.nombre_archivo, fd);
+	sock1.recibir_archivo(fd);
+	fd.close();
+	base_de_datos.renombrar_temporal(mod.nombre_archivo);
+	base_de_datos.registrar_nuevo(mod.nombre_archivo);
 	return true;
 }
+
 bool Controlador::enviar_nuevo_archivo(std::string& nombre_archivo){
-	// asumo que el archivo entra en ram (en un string)
-	// pide a base de datos el archivo en string
-	// pasa el archivo (que está en forma de string) al server
-	sock1.enviar_flag(ARCH_NUEVO);
+	// No asumo que el archivo entra en ram (en un string)
+	// pide a base de datos el archivo como fstream
+	// pasa el archivo al server
+	sock1.enviar_flag(ARCHIVO_ENTERO);
 	// ACA LE TENDRIA QUE PEDIR A LA BASE DE DATOS POR METODO QUE ABRA EL ARCHIVO
 	//ifstream fd(nombre_archivo.c_str(),ifstream::binary);
-	ifstream* fd = base_de_datos.abrir_para_leer(nombre_archivo);
-	sock1.enviar_archivo(*fd);
-	fd->close();
-	delete fd;
+	ifstream fd;
+	base_de_datos.abrir_para_leer(nombre_archivo, fd);
+	sock1.enviar_archivo(fd);
+	fd.close();
 	return true;
 }
+
 bool Controlador::modificar_archivo(std::string& nombre_archivo){
 	// le indica a la base de datos que modifique el archivo
 	// falta definir parametros de modificacion
@@ -73,38 +88,25 @@ bool Controlador::pedir_modificacion(std::string& nombre_archivo){
 	return true;
 }
 bool Controlador::aplicar_modificacion(Modificacion& mod) {
-	switch (mod.accion) {
-	case SUBIR_NUEVO_ARCHIVO: {
+	switch (mod.accion)
+	{
+	case SUBIR_NUEVO_ARCHIVO:
 		return enviar_nuevo_archivo(mod.nombre_archivo);
-		break;
-	}
-	case BAJAR_NUEVO_ARCHIVO: {
+	case BAJAR_NUEVO_ARCHIVO:
 		return pedir_nuevo_archivo(mod);
-		break;
-	}
-	case BORRAR_ARCHIVO_LOCAL: {
+	case BORRAR_ARCHIVO_LOCAL:
 		return borrar_archivo(mod.nombre_archivo);
-		break;
-	}
-	case MANDAR_A_BORRAR_ARCHIVO: {
+	case MANDAR_A_BORRAR_ARCHIVO:
 		return mandar_a_borrar(mod);
-		break;
-	}
-	case SUBIR_MOD_ARCHIVO: {
+	case SUBIR_MOD_ARCHIVO:
 		return enviar_modificacion(mod);
-		break;
-	}
-	case BAJAR_MOD_ARCHIVO: {
+	case BAJAR_MOD_ARCHIVO:
 		return pedir_modificacion(mod.nombre_archivo);
-		break;
-	}
 	}
 	return false;
 }
 
-
 std::vector<Modificacion> Controlador::comprobar_cambios_locales(){
-	return base_de_datos.comprobar_cambios_locales();
+        return base_de_datos.comprobar_cambios_locales();
 }
-
 
--- trunk/src/common_controlador.h
+++ trunk/src/common_controlador.h
@@ -1,14 +1,74 @@
-/*
- * common_controlador.h
- *
- *  Created on: 22/05/2013
- *      Author: jorge
- */
-
 #ifndef COMMON_CONTROLADOR_H_
 #define COMMON_CONTROLADOR_H_
+#include <string>
+#include <vector>
+#include "common_base_de_datos.h"
+#include "common_modificacion.h"
+#include "common_socket_prot.h"
+class Controlador {
+protected:
+	SocketProt sock1;
+	SocketProt sock2;
+	string dir;
+	BaseDeDatos base_de_datos;
+	bool terminar;
+public:
 
+	virtual void start() = 0;
+	/** @brief Setea el directorio en donde se guardan los archivos del usuario
+	 * OJO: usar antes del start()  */
+	void set_directorio(std::string dir);
 
+	/**@brief le pide a la base de datos que arme el indice local podria estar por defecto
+	 * en el constructor de la base de datos pero es mas legible si lo llamo explicitamente*/
+	bool armar_indice_local();
 
+	/**@brief pide lista de modificaciones al servidor
+	 */
+	std::vector<Modificacion> recibir_modificaciones();
+	/**@brief Recorre los archivos en busca de modificaciones a cada modificacion
+	 * encontrada se genera una nueva instancia de la clase Modificacion y se agrega
+	 * al vector
+	 */
+	std::vector<Modificacion> comprobar_cambios_locales();
+	/**@brief efectua una modificacion, segun los parametros de modificacion, la accion a realizar variarÃ¡
+	 */
+	bool aplicar_modificacion(Modificacion& mod);
+
+	/** @brief pide a la base de datos que borre el archivo
+	 */
+	bool borrar_archivo(std::string& nombre_archivo);
+	/**@brief envia un mensaje al otro extremo diciendo que se borre el archivo
+	 */
+	bool mandar_a_borrar(Modificacion& mod);
+	/**@brief pide el archivo cuyo nombre es ingresado al otro extremo
+	 * @details pide el archivo al otro extremo y lo guarda en su correspondiente lugar
+	 *  utilizando a la clase BaseDeDatos
+	 * @return devuelve true si toda la operacion fue un exito, caso contrario devuelve false
+	 */
+	bool pedir_nuevo_archivo(Modificacion& mod);
+	/**@brief enviar el archivo cuyo nombre es ingresado
+	 * @details el archivo a enviar serÃ¡ abierto y a medida que se lee se envia
+	 * @return devuelve true si la operacion fue un exito, caso contrario devuelve false
+	 */
+	bool enviar_nuevo_archivo(std::string& nombre_archivo);
+
+	/**@brief pide a la base de datos que modifique a un archvio
+	 */
+	bool modificar_archivo(std::string& nombre_archivo);
+
+	/**@brief envia un mensaje al server con los datos necesarios para realizar la
+	 *  modificacion del archivo ingresado
+	 */
+	bool enviar_modificacion(Modificacion& mod);
+
+	/**@brief pide al server la informacion necesaria para poder realizar la
+	 *  modificacion del archivo y lo modifica
+	 */
+	bool pedir_modificacion(std::string& nombre_archivo);
+
+	virtual ~Controlador() {
+	}
+};
 
 #endif /* COMMON_CONTROLADOR_H_ */

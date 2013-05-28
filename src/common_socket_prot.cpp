1d57026abda5b51a1020763a436e2e4381ef4a9e
cosokiter <cosokiter@gmail.com>
2013-05-28 12:10:59 +0000

    Implementado el enviar archivo del socket prot

--- src/common_socket_prot.cpp
+++ src/common_socket_prot.cpp
@@ -1,13 +1,16 @@
 #include "common_socket_prot.h"
 
+#define TAM_BUFFER 4096
 
 bool SocketProt::enviar_flag(const Packet flag)
 {
+	if (enviar((void*) &flag, 1) <= 0) return false;
 	return true;
 }
 
 bool SocketProt::recibir_flag(Packet &flag)
 {
+	if (recibir((void*) &flag, 1) <= 0) return false;
 	return true;
 }
 
@@ -30,11 +33,36 @@ bool SocketProt::recibir_pedazo_archivo(ofstream &arch, size_t offset, size_t le
 {
 	return true;
 }
-bool SocketProt::enviar_archivo(ifstream &arch){
 
+bool SocketProt::enviar_archivo(ifstream &arch)
+{
+	//TODO: Optimizar viendo como funciona el buffer interno de ifstream
+	arch.seekg(0,ios::end);
+	streampos fin = arch.tellg();
+	arch.seekg(0);
+	char buffer[TAM_BUFFER];
+	while (fin > 0)
+	{
+		streamsize aEnviar = TAM_BUFFER;
+		if (fin < TAM_BUFFER) aEnviar = fin;
+		arch.read(buffer, aEnviar);
+		if (!enviarLen(buffer, aEnviar)) return false;
+		fin -= aEnviar;
+	}
 	return true;
 }
-bool SocketProt::recibir_archivo(std::ofstream& fd){
 
+bool SocketProt::recibir_archivo(std::ofstream &arch, streamsize tam)
+{
+	//TODO: Optimizar viendo como funciona el buffer interno de ofstream
+	char buffer[TAM_BUFFER];
+	while (tam > 0)
+	{
+		streamsize aRecibir = TAM_BUFFER;
+		if (tam < TAM_BUFFER) aRecibir = tam;
+		if (!recibirLen(buffer, aRecibir)) return false;
+		arch.write(buffer, aRecibir);
+		tam -= aRecibir;
+	}
 	return true;
 }

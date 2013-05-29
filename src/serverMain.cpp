c1e931006f12c8c8c713f2a4265c728d8859ccdb
collinetjorge <collinetjorge@gmail.com>
2013-05-29 05:14:56 +0000

    subo clase BaseDeDatosUsuario completamente funcional y probada, subo tambien una prueba.

--- src/serverMain.cpp
+++ src/serverMain.cpp
@@ -1,4 +1,10 @@
+//#define server_main
+#ifndef server_main
 int main (int argc, char** argv)
 {
+
+
+	//
 	return 0;
 }
+#endif
---
+++ src/server_prueba_base_de_datos_usuario.cpp
@@ -0,0 +1,34 @@
+
+#define prueba_de_base_de_datos_usuario
+#ifndef prueba_de_base_de_datos_usuario
+#include "server_base_de_datos_usuario.h"
+#include <iostream>
+using namespace std;
+int main(int args, char** argv){
+
+	BaseDeDatosUsuario Bdd("/home/jorge/workspace2/tp5 tallerg/src/base_de_datos_usuario");
+	// !los nombres sin espacios!!
+	Bdd.agregar_usuario("pepito","xlanoche");
+	Bdd.agregar_usuario("juanadearco", "tengounarcho");
+	cout<<"tiene que decir 1(true) : "<<Bdd.usuario_contrasenia_correcto("pepito","xlanoche")<<endl;
+
+	Bdd.guardar_y_cerrar();
+
+	BaseDeDatosUsuario Bdd2("/home/jorge/workspace2/tp5 tallerg/src/base_de_datos_usuario");
+	Bdd2.abrir();
+	cout<<"tiene que decir 1(true) : "<<Bdd2.usuario_contrasenia_correcto("pepito","xlanoche")<<endl;
+	cout<<"tiene que decir 0(false) : "<<Bdd2.usuario_contrasenia_correcto("shakira","tumama")<<endl;
+	Bdd2.agregar_usuario("laaaaa","la");
+	Bdd2.guardar_y_cerrar();
+
+	BaseDeDatosUsuario Bdd3("/home/jorge/workspace2/tp5 tallerg/src/base_de_datos_usuario");
+	Bdd3.abrir();
+	Bdd3.eliminar_usuario("pepito");
+	Bdd3.guardar_y_cerrar();
+
+
+}
+
+
+
+#endif

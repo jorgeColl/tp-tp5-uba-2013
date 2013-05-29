748895bb7fb10a3c6b3da3181d4321d4d7cb52b7
collinetjorge@gmail.com <collinetjorge@gmail.com@b2cfa7c8-117a-d20a-678d-662d165223aa>
2013-05-29 20:42:06 +0000

    guau !!! modificador ha subido de nivel!!! ahora puede mostrarse en pantalla !!!!

--- trunk/src/common_modificacion.cpp
+++ trunk/src/common_modificacion.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "common_modificacion.h"
+
 using namespace std;
 
 Modificacion::Modificacion(tipo_accion accion,string nombre_archivo,size_t bloque_ini, size_t bloque_fin)
@@ -14,3 +15,31 @@ Modificacion::Modificacion(tipo_accion accion,string nombre_archivo,size_t bloqu
 	this->bloque_fin = bloque_fin;
 }
 
+/**@brief muestra el estado interno del string */
+ostream& operator<<(ostream& os, Modificacion& mod) {
+	// coso asqueroso para que imprima en palabras y no en numeros
+	string accion;
+	switch (mod.accion) {
+	case (SUBIR_NUEVO_ARCHIVO):
+		accion = "SUBIR_NUEVO_ARCHIVO";
+		break;
+	case (BAJAR_NUEVO_ARCHIVO):
+		accion = "BAJAR_NUEVO_ARCHIVO";
+		break;
+	case (BORRAR_ARCHIVO_LOCAL):
+		accion = "BORRAR_ARCHIVO_LOCAL";
+		break;
+	case (MANDAR_A_BORRAR_ARCHIVO):
+		accion = "MANDAR_A_BORRAR_ARCHIVO";
+		break;
+	case (SUBIR_MOD_ARCHIVO):
+		accion = "SUBIR_MOD_ARCHIVO";
+		break;
+	case (BAJAR_MOD_ARCHIVO):
+		accion = "BAJAR_MOD_ARCHIVO";
+		break;
+	}
+	os << "Accion: "<<mod.accion <<" Nombre de archivo: "<<mod.nombre_archivo<<endl;
+	os <<"Bloque inicial: "<<mod.bloque_ini<<" Bloque final: "<<mod.bloque_fin;
+	return os ;
+}

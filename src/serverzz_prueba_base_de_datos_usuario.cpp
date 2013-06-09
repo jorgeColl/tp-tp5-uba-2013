6adf432c486ba3ac20b44d9d941497563084b5d9
collinetjorge <collinetjorge@gmail.com>
2013-06-09 18:48:18 +0000

    cambios en arch_mutexcer

--- src/serverzz_prueba_base_de_datos_usuario.cpp
+++ src/serverzz_prueba_base_de_datos_usuario.cpp
@@ -12,19 +12,20 @@ int main(int args, char** argv){
 	Bdd.agregar_usuario("juanadearco", "tengounarcho");
 	cout<<"tiene que decir 1(true) : "<<Bdd.usuario_contrasenia_correcto("pepito","xlanoche")<<endl;
 
-	Bdd.guardar_y_cerrar();
+	Bdd.guardar_a_disco();
 
 	BaseDeDatosUsuario Bdd2("/home/jorge/workspace2/tp5 tallerg/src/base_de_datos_usuario");
 	Bdd2.abrir();
 	cout<<"tiene que decir 1(true) : "<<Bdd2.usuario_contrasenia_correcto("pepito","xlanoche")<<endl;
 	cout<<"tiene que decir 0(false) : "<<Bdd2.usuario_contrasenia_correcto("shakira","tumama")<<endl;
 	Bdd2.agregar_usuario("laaaaa","la");
-	Bdd2.guardar_y_cerrar();
+	Bdd2.guardar_a_disco();
 
 	BaseDeDatosUsuario Bdd3("/home/jorge/workspace2/tp5 tallerg/src/base_de_datos_usuario");
 	Bdd3.abrir();
 	Bdd3.eliminar_usuario("pepito");
-	Bdd3.guardar_y_cerrar();
+	// simplemente dejo que se elimine solo para no guardar los cambios a disco
+	//Bdd3.guardar_a_disco();
 
 
 }
